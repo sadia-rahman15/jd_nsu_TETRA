651ba86a6ed31f66848c0e19ef11adde27974f22
Tanvir-Miraz <119744418+Tanvir-Miraz@users.noreply.github.com>
2026-08-17 07:36:18 +0600

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,1 +1,23 @@
 
+import { Stack } from "expo-router";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
+
+export default function Layout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
+}
